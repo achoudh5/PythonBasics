5ca8bfd515ae5ea0f9ef5babd274e5de6854e661
anschoud <achoudh5@ncsu.edu>
2019-02-12 01:24:24 -0500

    sahi

--- Q1.py
+++ Q1.py
@@ -1,4 +1,9 @@
-#Q:- What to enter in the function var while calling a function ex. here I have put a random number
+#1. Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+# between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+
+#Hints: Consider use range(#begin, #end) method
+#Q1:- What to enter in the function var while calling a function ex. here I have put a random number
 #inside q1(0) to call the function, what else can I do?
 #Q2: why return gives nothing but print gives me the list output.
 def q1(a):
