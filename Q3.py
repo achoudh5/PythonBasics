841773d88c7f2d097c4dd750145361ca6d479f8a
anschoud <achoudh5@ncsu.edu>
2019-02-13 16:51:51 -0500

    Q3

---
+++ Q3.py
@@ -0,0 +1,9 @@
+#3. With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included) and then the program should print the dictionary.
+
+def q3(n):
+    d={}
+    for i in range(1,n+1):
+        d[int(i)] = int(i*i)
+    print d
+a=int(raw_input("Enter a number:"))
+q3(a)
