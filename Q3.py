d62f896cc038d730b1bd5441ad9d0394dbd380a7
Anshul Choudhary <34908313+achoudh5@users.noreply.github.com>
2019-02-13 16:53:32 -0500

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,4 +1,5 @@
-#3. With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included) and then the program should print the dictionary.
+#3. With a given integral number n, write a program to generate a dictionary that contains (i, i*i) 
+#such that is an integral number between 1 and n (both included) and then the program should print the dictionary.
 
 def q3(n):
     d={}
