e9b7bdd26651b9d8a50bcc1a6e2f8470302e2050
anschoud <achoudh5@ncsu.edu>
2019-02-16 18:57:06 -0500

    Q8

---
+++ Q9.py
@@ -0,0 +1,4 @@
+#Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized.
+
+def q9(self):
+    a= raw_input("enter the string :\n")
