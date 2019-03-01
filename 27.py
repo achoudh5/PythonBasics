9eb18b9817f2daeae648eb882cff3bd6792afe23
anschoud <achoudh5@ncsu.edu>
2019-03-01 02:37:48 -0500

    @staticmethod (no args is given while calling the function)

--- 27.py
+++ 27.py
@@ -1,7 +1,9 @@
 #Q25
 
-class A:
-    def ashu(self):
+class American:
+
+    #@staticmethod
+    def printNationality(self):
         #dict=[]
         #for i in range(1,21):
         #   dict.append(i)
@@ -11,8 +13,28 @@ class A:
 
 
 def main():
-    l = A()
-    l.ashu()
+    l = American()
+    l.printNationality()
+
+
+if __name__=="__main__":
+    main()
+
+class American:
+
+    @staticmethod
+    def printNationality():
+        #dict=[]
+        #for i in range(1,21):
+        #   dict.append(i)
+        #print dict
+        z= list(map(lambda x: x**2,range(1,21)))
+        print z
+
+
+def main():
+    l = American()
+    l.printNationality()
 
 
 if __name__=="__main__":
