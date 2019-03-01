be8cc27a5f18073acb8b97f543fad1aa940ae994
anschoud <achoudh5@ncsu.edu>
2019-03-01 16:23:40 -0500

    class and subclass

--- 27.py
+++ 27.py
@@ -1,40 +1,29 @@
-#Q25
-
-class American:
+from math import *
+class Circle:
 
     #@staticmethod
-    def printNationality(self):
+    def __init__(self):
         #dict=[]
         #for i in range(1,21):
         #   dict.append(i)
         #print dict
-        z= list(map(lambda x: x**2,range(1,21)))
-        print z
+        #self.radius=radius
+        pass
+    def rec(self):
+        print 0
 
+class Square(Circle):
+    def __init__(self,l):
+        self.l=l
 
+    def rec(self):
+        print self.l * self.l
 def main():
-    l = American()
-    l.printNationality()
-
-
-if __name__=="__main__":
-    main()
-
-class American:
-
-    @staticmethod
-    def printNationality():
-        #dict=[]
-        #for i in range(1,21):
-        #   dict.append(i)
-        #print dict
-        z= list(map(lambda x: x**2,range(1,21)))
-        print z
-
-
-def main():
-    l = American()
-    l.printNationality()
+    d=int(raw_input("enter the number"))
+    l = Square(d)
+    #j=NewYorker()
+    l.rec()
+    #j.printNationality()
 
 
 if __name__=="__main__":
