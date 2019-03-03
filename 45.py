89e5c3f22162d4274b5195e2b19a03dd6b954704
anschoud <achoudh5@ncsu.edu>
2019-03-03 13:39:52 -0500

    print char with even indexes using extended slicing

---
+++ 45.py
@@ -0,0 +1,23 @@
+import random
+from random import shuffle
+import zlib
+import sys
+import timeit
+class Circle:
+    def rec(self):
+
+        a= raw_input("Enter the string\n")
+        print a[::2]   #print char with even indexes
+
+
+
+
+
+def main():
+    l = Circle()
+
+    # j=NewYorker()
+
+    l.rec()
+if __name__=="__main__":
+    main()
