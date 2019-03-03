a16d053e922a2a265bfa672637e317c9a73abef0
anschoud <achoudh5@ncsu.edu>
2019-03-03 13:46:00 -0500

    import itertools itertools.permutations(<list>)

---
+++ 46.py
@@ -0,0 +1,26 @@
+import random
+from random import shuffle
+import zlib
+import sys
+import timeit
+import itertools
+
+class Circle:
+    def rec(self):
+
+        a= [1,2,3]
+        print list(itertools.permutations(a)) #using permutations to print all the combinations
+
+
+
+
+
+
+def main():
+    l = Circle()
+
+    # j=NewYorker()
+
+    l.rec()
+if __name__=="__main__":
+    main()
