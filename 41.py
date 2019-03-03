b0463e918108bcd2dbb1f46a94f178ec40a842a1
anschoud <achoudh5@ncsu.edu>
2019-03-02 20:16:22 -0500

    remove an element from the list using list comprehension

---
+++ 41.py
@@ -0,0 +1,23 @@
+import random
+from random import shuffle
+import zlib
+import sys
+import timeit
+class Circle:
+    def rec(self):
+
+        a=[12,24,35,24,88,120,155]
+        print [i for i in a if i!=24]
+
+
+
+
+
+def main():
+    l = Circle()
+
+    # j=NewYorker()
+
+    l.rec()
+if __name__=="__main__":
+    main()
