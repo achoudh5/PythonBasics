1a0fea18bf5b02c67f9e920c773fb1af0d29f357
anschoud <achoudh5@ncsu.edu>
2019-03-02 20:45:35 -0500

    duplicate items removal using the list comprehension

---
+++ 42.py
@@ -0,0 +1,25 @@
+import random
+from random import shuffle
+import zlib
+import sys
+import timeit
+import collections
+class Circle:
+    def rec(self):
+        list= [12, 24, 35, 24, 88, 120, 155, 88, 120, 155]
+        b=[]
+        print [b.append(i) for i in list if i not in b]
+        print b
+
+
+
+
+
+def main():
+    l = Circle()
+
+    # j=NewYorker()
+
+    l.rec()
+if __name__=="__main__":
+    main()
