d399bd03f033a058dc761dbdd76ceb9d5968a451
anschoud <achoudh5@ncsu.edu>
2019-03-02 19:54:17 -0500

    3-D array using lsit cmprehension

---
+++ 39.py
@@ -0,0 +1,26 @@
+import random
+from random import shuffle
+import zlib
+import sys
+import timeit
+class Circle:
+    def rec(self):
+
+        a=[0,0,0]
+        b=[0,0,0,0,0]
+        c=[0,0,0,0,0,0,0,0]
+        array = [[ [0 for col in range(8)] for col in range(5)] for row in range(3)]
+        print array
+
+
+
+
+
+def main():
+    l = Circle()
+
+    # j=NewYorker()
+
+    l.rec()
+if __name__=="__main__":
+    main()
