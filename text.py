49fe72ad2ba221218019d4c65afd2f4d1c3b30ac
anschoud <achoudh5@ncsu.edu>
2019-03-05 01:17:27 -0500

    SE2

---
+++ text.py
@@ -0,0 +1,3 @@
+dict= {'Nikhil':{ 'Name': 'Nikhil Kamat' , 'Contact' :'(919)272-9000' , 'Emailid' :'nikhilkamat2@gmail.com' , 'Department' :'Computer Networking'
+},'Anshul':{ 'Name': 'Anshul Choudhary' , 'Contact': '(919)272-9112' , 'Emailid': 'achoudh5@ncsu.edu' , 'Department': 'Computer Networking'},'Vaishnavi':{ 'Name': 'Vaishnavi Voraiswamy',
+'Contact': '(919)272-0900', 'Emailid' : 'vdorais@ncsu.edu' , 'Department': 'Computer Networking' }}
